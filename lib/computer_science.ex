3c9ccd22b72cc3a18cc86c27d1d929bcb885a07f
Tsukasa Arima <pyar6329@gmail.com>
2017-12-14 15:01:54 +0900

    wip

--- lib/computer_science.ex
+++ lib/computer_science.ex
@@ -1,4 +1,7 @@
 defmodule ComputerScience do
+  @expression "1+2*3-4*5"
+  @rpn "123*45*-+"
+
   def push(stack, args) do
     [args | stack]
   end
@@ -7,4 +10,23 @@ defmodule ComputerScience do
     [head | tail] = stack
     {head, tail}
   end
+
+  def reverse_polish_notation(args \\ @expression) do
+    exp_list = args |> String.codepoints()
+    figures = []
+    signs = []
+  end
+
+  def stack(args) do
+    case args do
+      "+" -> ""
+      "-" -> ""
+      "*" -> ""
+      _ -> args |> String.to_integer()
+    end
+  end
+
+  def aaa(args) do
+
+  def aaa([]) do: ""
 end
